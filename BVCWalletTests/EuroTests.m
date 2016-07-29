186ca882d6963d47c27c82b86d30bb45c51681fe
Bhavish Chandnani <bhavish.chandnani@gmail.com>
2016-07-29 21:11:04 +0200

    Created Test for Euro and added some tests (multiplication and equality)

---
+++ BVCWalletTests/EuroTests.m
@@ -0,0 +1,46 @@
+//
+//  EuroTests.m
+//  BVCWallet
+//
+//  Created by Bhavish Chandnani on 29/7/16.
+//  Copyright © 2016 BVC. All rights reserved.
+//
+
+@import XCTest;
+
+#import "Euro.h"
+
+@interface EuroTests : XCTestCase
+
+@end
+
+@implementation EuroTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testMultiplication{
+    Euro *euro = [[Euro alloc] initWithAmount: 5];
+    Euro *total =  [euro times:2];
+    
+    XCTAssertEqual(total.amount, 10, @"Both should be equal");
+    
+}
+
+-(void) testEquality{
+    Euro *five = [[Euro alloc] initWithAmount:5];
+    Euro *ten = [[Euro alloc] initWithAmount:10];
+    Euro *total = [five times: 2];
+    
+    XCTAssertEqualObjects(ten, total, @"Equivalent objects shoul be equal");
+    
+}
+
+@end
